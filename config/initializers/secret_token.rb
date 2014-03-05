dfe5fdb890d271b1322a8d3a40cbccab6a01e23e
Meg303 <megrhinehart@gmail.com>
2014-03-05 18:40:40 -0500

    adding my repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pushto::Application.config.secret_key_base = '58d7776adc67a52af6d66a218dec563c01cecd7855f20ddeb4f65ba95f73e60e45cd25d31bb8ca647aa0c535be376b9a44444e3cc4f40d938cfd8d891e25016a'
